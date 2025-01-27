886191729f2cf70031de2cda3226095d914832ce
Joao Neto <chomcinho@gmail.com>
2025-01-27 11:44:47 +0100

    JS funcionando e site pronto

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,12 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}`);
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.add("invalido");
   errorElement.textContent = errorMessage;
   errorElement.classList.add("popup__msg-error");
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}`);
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.remove("invalido");
   errorElement.classList.remove("popup__msg-error");
   errorElement.textContent = "";
@@ -28,17 +28,17 @@ const hasInvalidInput = (inputList) => {
 
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("popup__save-button:disabled");
+    buttonElement.classList.add("popup__save-button_disabled");
     buttonElement.disabled = true;
   } else {
-    buttonElement.classList.remove("popup__save-button:disabled");
+    buttonElement.classList.remove("popup__save-button_disabled");
     buttonElement.disabled = false;
   }
 };
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-  const buttonElement = formElement.querySelector(".popup__button");
+  const buttonElement = formElement.querySelector(".popup__save-button");
 
   toggleButtonState(inputList, buttonElement);
 
@@ -52,6 +52,7 @@ const setEventListeners = (formElement) => {
 
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll(".popup__form"));
+  console.log(formList);
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
